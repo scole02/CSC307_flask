f0c97d61930681657848805d294531635048464d
scole02 <samcole107@gmail.com>
2021-04-11 22:15:45 -0700

    successful POST now returns code 201

--- sample_backend.py
+++ sample_backend.py
@@ -61,7 +61,7 @@ def get_users():
       userToAdd = request.get_json()
       users['users_list'].append(userToAdd)
       resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
+      resp.status_code = 201 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
    elif request.method == 'DELETE':
