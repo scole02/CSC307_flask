f1b831b2141444ecfd1dc84a872caa9e7c0f2cda
scole02 <samcole107@gmail.com>
2021-04-11 22:51:11 -0700

    POST method now returns character json with new id

--- sample_backend.py
+++ sample_backend.py
@@ -61,7 +61,7 @@ def get_users():
       userToAdd = request.get_json()
       userToAdd["id"] = random_id()
       users['users_list'].append(userToAdd)
-      resp = jsonify(success=True)
+      resp = jsonify(success=True, data=userToAdd)
       resp.status_code = 201 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
